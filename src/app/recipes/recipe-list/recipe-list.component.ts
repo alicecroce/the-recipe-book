bced833bc9f29aed55bd94b3814744721f17b6dd
alicecroce <alicecroce2@gmail.com>
2024-06-04 10:19:41 +0200

    Single recipe configuration in the html

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-  recipes=[];
+  recipes:Recipe[]=[
+    new Recipe('A Test Recipe', 'This is a semply Test', 'https://get.pxhere.com/photo/dish-food-cuisine-ingredient-Karedok-meat-donburi-produce-side-dish-dak-galbi-Cao-l-u-yaki-udon-recipe-thai-food-oyakodon-korean-food-1614584.jpg')
+  ];
 
 }
